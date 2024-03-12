4083d5cc70bc094561186f513cc1583ad0e77123
Gregors Lasenbergs <gregors.las@gmail.com>
2024-03-12 11:16:00 +0100

    Added search and sort functionality

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,17 +1,60 @@
-import axios from "axios";
+import axios from 'axios';
 
 const countryList = document.querySelector('.country-list');
+const searchInput = document.querySelector('.search-bar');
+const allSortButtons = document.querySelectorAll('.btn');
+const sortByName = allSortButtons[0];
+const sortByCapital = allSortButtons[1];
+const sortByRegion = allSortButtons[2];
+const sortByLanguage = allSortButtons[3];
+const sortByCurrency = allSortButtons[4];
+
+// Sort button that sorts ascending, but if clicked again, sorts descending
+
+let ascendingName: boolean;
+let ascendingCapital: boolean;
+let ascendingRegion: boolean;
+let ascendingLanguage: boolean;
+let ascendingCurrency: boolean;
 
 type Country = {
   name: string;
   capital: string;
-  population: number;
   region: string;
+  language: {
+    name: string;
+    code: string;
+  };
+  currency: {
+    code: string;
+    name: string;
+    symbol: string;
+  };
 }
 
-const fetchData = async () => {
-  const taskUrl:string = 'http://localhost:3004/countries?_limit=20';
-  let countries: Country[] = [];
+const addCountryElement = (newCountry: Country) => {
+  const country = document.createElement('div');
+  // const name = newCountry.name;
+  country.className = 'country-list__country';
+  country.innerHTML = `
+    <p class="field">${newCountry.name}</p>
+    <p class="field">${newCountry.capital}</p>
+    <p class="field">${newCountry.region}</p>
+    <p class="field">${newCountry.language.name}</p>
+    <p class="field">${newCountry.currency.name}</p>`;
+  countryList.append(country);
+};
+
+const updateCountryList = (countries: Country[]) => {
+  countryList.innerHTML = '';
+  countries.forEach((country: Country) => {
+    addCountryElement(country);
+  });
+};
+
+const fetchData = async (url: string) => {
+  const taskUrl:string = url;
+  const countries: Country[] = [];
   try {
     const response = await axios.get(taskUrl);
     response.data.forEach((element: Country) => {
@@ -25,23 +68,78 @@ const fetchData = async () => {
   }
 };
 
-const addCountryElement = (newCountry: Country) => {
-  let country = document.createElement('div');
-  // const name = newCountry.name;
-  country.className = 'country-list__country';
-  country.innerHTML = `
-    <p class="field">${newCountry.name}</p>
-    <p class="field">Capital: ${newCountry.capital}</p>
-    <p class="field">${newCountry.region}</p>`;
-  countryList.append(country);
-};
+sortByName.addEventListener('click', async () => {
+  let sortedCountries: Country[];
+  if (ascendingName) {
+    sortedCountries = await fetchData('http://localhost:3004/countries?_sort=name&_limit=20');
+    ascendingName = false;
+  } else {
+    sortedCountries = await fetchData('http://localhost:3004/countries?_sort=name&_order=desc&_limit=20');
+    ascendingName = true;
+  }
+  updateCountryList(sortedCountries);
+});
+
+sortByCapital.addEventListener('click', async () => {
+  let sortedCountries: Country[];
+  if (ascendingCapital) {
+    sortedCountries = await fetchData('http://localhost:3004/countries?_sort=capital&_limit=20');
+    ascendingCapital = false;
+  } else {
+    sortedCountries = await fetchData('http://localhost:3004/countries?_sort=capital&_order=desc&_limit=20');
+    ascendingCapital = true;
+  }
+  updateCountryList(sortedCountries);
+});
+
+sortByRegion.addEventListener('click', async () => {
+  let sortedCountries: Country[];
+  if (ascendingRegion) {
+    sortedCountries = await fetchData('http://localhost:3004/countries?_sort=region&_limit=20');
+    ascendingRegion = false;
+  } else {
+    sortedCountries = await fetchData('http://localhost:3004/countries?_sort=region&_order=desc&_limit=20');
+    ascendingRegion = true;
+  }
+  updateCountryList(sortedCountries);
+});
+
+sortByLanguage.addEventListener('click', async () => {
+  let sortedCountries: Country[];
+  if (ascendingLanguage) {
+    sortedCountries = await fetchData('http://localhost:3004/countries?_sort=language.name&_limit=20');
+    ascendingLanguage = false;
+  } else {
+    sortedCountries = await fetchData('http://localhost:3004/countries?_sort=language.name&_order=desc&_limit=20');
+    ascendingLanguage = true;
+  }
+  updateCountryList(sortedCountries);
+});
+
+sortByCurrency.addEventListener('click', async () => {
+  let sortedCountries: Country[];
+  if (ascendingCurrency) {
+    sortedCountries = await fetchData('http://localhost:3004/countries?_sort=currency.name&_limit=20');
+    ascendingCurrency = false;
+  } else {
+    sortedCountries = await fetchData('http://localhost:3004/countries?_sort=currency.name&_order=desc&_limit=20');
+    ascendingCurrency = true;
+  }
+  updateCountryList(sortedCountries);
+});
 
 const loadAllCardsFromDb = (countries: Promise<Country[] | any[]>) => {
-  countries.then((countries: Country[]) => {
-    countries.forEach((card: Country) => {
+  countries.then((entry: Country[]) => {
+    entry.forEach((card: Country) => {
       addCountryElement(card);
     });
   });
 };
 
-loadAllCardsFromDb(fetchData());
+searchInput.addEventListener('input', async (event) => {
+  const searchValue = (event.target as HTMLInputElement).value;
+  const filteredCountries: Country[] = await fetchData(`http://localhost:3004/countries?q=${searchValue}&_limit=20`);
+  updateCountryList(filteredCountries);
+});
+
+loadAllCardsFromDb(fetchData('http://localhost:3004/countries?_limit=20'));
